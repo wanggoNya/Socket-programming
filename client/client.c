0e2fa54920d0beb1570f3af55b5d6ac366310adf
wanggoNya <tndus6384@naver.com>
2022-10-17 18:26:01 +0900

    client message code

--- client/client.c
+++ client/client.c
@@ -10,11 +10,13 @@ int main(int argc, char* argv[])
 		int my_sock;
 		struct sockaddr_in serv_addr;
 		int str_len;
+
 		if(argc != 3)
 		{
 				printf("%s <IP> <PORT>\n", argv[0]);
 				exit(1);
 		}
+
 		my_sock = socket(PF_INET,SOCK_STREAM,0); //1번
 		if(my_sock == -1)
 				printf("socket error \n");
@@ -25,11 +27,16 @@ int main(int argc, char* argv[])
 
 		if(connect(my_sock,(struct sockaddr*)&serv_addr,sizeof(serv_addr))==-1) //2번
 				printf("connect error\n");
+
 		char message[15];
+
 		str_len = read(my_sock,message,sizeof(message)-1); //3번
+
 		if(str_len==-1)
 				printf("read error\n");
+
 		printf("서버에서 : %s \n", message);
+
 		close(my_sock); //4번		
 		return 0;
 }
