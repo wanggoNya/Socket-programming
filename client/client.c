f9684ec91ac83bbdcdc7499e5cc5d05c620524e8
wanggoNya <tndus6384@naver.com>
2022-10-17 15:47:16 +0900

    client code

--- client/client.c
+++ client/client.c
@@ -1,4 +1,9 @@
-#include "client.h"
+#include <stdio.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char* argv[])
 {
