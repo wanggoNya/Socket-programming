a960aa1b92eb94982cd0a2ca27f2fda6672ee635
wanggoNya <tndus6384@naver.com>
2022-10-18 13:56:17 +0900

    code 분석

--- clientMsg/client.c
+++ clientMsg/client.c
@@ -7,67 +7,64 @@
 #include <sys/socket.h>
 #include <unistd.h> // read(), write(), close()
 #define MAX 80
-#define PORT 8027 
 #define SA struct sockaddr
-static void func(int sockfd)
+
+static void chat(int clntfd)
 {
-    char buff[MAX];
-    int n;
-    for (;;) {
-        bzero(buff, sizeof(buff));
-        printf("Enter the string : ");
-        n = 0;
-        while ((buff[n++] = getchar()) != '\n')
-            ;
-        write(sockfd, buff, sizeof(buff));
-        bzero(buff, sizeof(buff));
-        read(sockfd, buff, sizeof(buff));
-        printf("From Server : %s", buff);
-        if ((strncmp(buff, "exit", 4)) == 0) {
-            printf("Client Exit...\n");
-            break;
-        }
-    }
+		char buff[MAX];
+		int n;
+		while(1) {
+				bzero(buff, sizeof(buff));
+				printf("Enter the string : ");
+
+				n = 0;
+				while ((buff[n++] = getchar()) != '\n');
+
+				write(clntfd, buff, sizeof(buff));
+
+				if ((strncmp(buff, "exit", 4)) == 0) {
+						printf("Client Exit...\n");
+						break;
+				}
+
+				bzero(buff, sizeof(buff));
+				read(clntfd, buff, sizeof(buff));
+				printf("From Server : %s", buff);
+		}
 }
- 
+
 int main(int argc, char* argv[])
 {
-    int sockfd;
-    struct sockaddr_in servaddr;
- 
-	if(argc != 3)
-	{
-			printf("%s <IP> <PORT>\n", argv[0]);
-			exit(1);
-	}
+		int clntfd;
+		struct sockaddr_in servaddr;
 
-    // socket create and verification
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd == -1) {
-        printf("socket creation failed...\n");
-        exit(0);
-    }
-    else
-        printf("Socket successfully created..\n");
-    bzero(&servaddr, sizeof(servaddr));
- 
-    // assign IP, PORT
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = inet_addr(argv[1]);
-    servaddr.sin_port = htons(atoi(argv[2]));
- 
-    // connect the client socket to server socket
-    if (connect(sockfd, (SA*)&servaddr, sizeof(servaddr))
-        != 0) {
-        printf("connection with the server failed...\n");
-        exit(0);
-    }
-    else
-        printf("connected to the server..\n");
- 
-    // function for chat
-    func(sockfd);
- 
-    // close the socket
-    close(sockfd);
+		if(argc != 3)
+		{
+				printf("%s <IP> <PORT>\n", argv[0]);
+				exit(1);
+		}
+
+		clntfd = socket(PF_INET, SOCK_STREAM, 0);
+		if (clntfd == -1) {
+				printf("socket creation failed...\n");
+				exit(0);
+		}
+		else printf("Socket successfully created..\n");
+
+		bzero(&servaddr, sizeof(servaddr));
+
+		servaddr.sin_family = AF_INET;
+		servaddr.sin_addr.s_addr = inet_addr(argv[1]);
+		servaddr.sin_port = htons(atoi(argv[2]));
+
+		if (connect(clntfd, (SA*)&servaddr, sizeof(servaddr))
+						!= 0) {
+				printf("connection with the server failed...\n");
+				exit(0);
+		}
+		else printf("connected to the server..\n");
+
+		chat(clntfd);
+
+		close(clntfd);
 }
